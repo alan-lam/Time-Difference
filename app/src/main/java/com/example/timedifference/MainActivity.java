75d62e01d851b1170180ef8315b4bc1d64f37b80
alan-lam <alanlam00@gmail.com>
2019-01-30 22:29:07 -0800

    some logic

--- app/src/main/java/com/example/timedifference/MainActivity.java
+++ app/src/main/java/com/example/timedifference/MainActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     EditText startMinuteEditText;
     EditText endHourEditText;
     EditText endMinuteEditText;
+    RadioButton startAMButton;
+    RadioButton endAMButton;
+    RadioButton startPMButton;
+    RadioButton endPMButton;
+    TextView displayTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
         startMinuteEditText = findViewById(R.id.start_minutes);
         endHourEditText = findViewById(R.id.end_hour);
         endMinuteEditText = findViewById(R.id.end_minutes);
+        startAMButton = findViewById(R.id.start_am);
+        endAMButton = findViewById(R.id.end_am);
+        startPMButton = findViewById(R.id.start_pm);
+        endPMButton = findViewById(R.id.end_pm);
+        displayTextView = findViewById(R.id.display);
     }
 
     public void calculate(View view) {
@@ -45,6 +59,21 @@ public class MainActivity extends AppCompatActivity {
         else if (startMinute > 59 || endMinute > 59) {
             Toast.makeText(this, "Enter minutes between 0 and 59", Toast.LENGTH_SHORT).show();
         }
+
+        /* 11:00 PM - 1:00 AM -> 11:00 PM - 13:00 AM or 11:00 AM - 1:00 PM -> 11:00 AM - 13:00 PM*/
+        if ((startPMButton.isChecked() && endAMButton.isChecked()) || (startAMButton.isChecked() && endPMButton.isChecked())) {
+            endHour += 12;
+        }
+
+        /* 12:34 PM - 1:02 PM -> 12:34 PM - 12:62 PM (28 minutes) */
+        if (endMinute < startMinute) {
+            endMinute += 60;
+            endHour -= 1;
+        }
+
+        int hourDifference = endHour - startHour;
+        int minuteDifference = endMinute - startMinute;
+
     }
 
     public void clear(View view) {
